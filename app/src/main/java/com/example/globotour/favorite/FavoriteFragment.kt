f3adb8e0c47c14c56fcd71b19b0cd3a525659949
LEONE <leonwahungu@gmail.com>
2023-02-27 12:38:15 +0300

    First commit of globotour app

---
+++ app/src/main/java/com/example/globotour/favorite/FavoriteFragment.kt
@@ -0,0 +1,89 @@
+package com.example.globotour.favorite
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.globotour.R
+import com.example.globotour.city.City
+import com.example.globotour.city.VacationSpots
+import com.google.android.material.snackbar.Snackbar
+import java.util.Collections
+
+class FavoriteFragment : Fragment() {
+    private lateinit var favoriteCityList: ArrayList<City>
+    private lateinit var favoriteAdapter: FavoriteAdapter
+    private lateinit var recyclerView: RecyclerView
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_favorite, container, false)
+
+        setupRecyclerView(view)
+
+        return view
+    }
+
+    private fun setupRecyclerView(view: View) {
+        val context = requireContext()
+        favoriteCityList = VacationSpots.favoriteCityList as ArrayList<City>
+        favoriteAdapter = FavoriteAdapter(context, favoriteCityList)
+
+        recyclerView = view.findViewById<RecyclerView>(R.id.favorite_recycler_view)
+        recyclerView.adapter = favoriteAdapter
+        recyclerView.setHasFixedSize(true)
+
+        val layoutManager = LinearLayoutManager(context)
+        layoutManager.orientation = RecyclerView.VERTICAL
+        recyclerView.layoutManager = layoutManager
+
+        itemTouchHelper.attachToRecyclerView(recyclerView)
+    }
+
+    private var itemTouchHelper = ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(ItemTouchHelper.UP or ItemTouchHelper.DOWN, ItemTouchHelper.RIGHT or ItemTouchHelper.LEFT) {
+        override fun onMove(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, targetViewHolder: RecyclerView.ViewHolder): Boolean {
+            //called when item is dragged
+            val fromPosition = viewHolder.adapterPosition
+            val toPosition = targetViewHolder.adapterPosition
+
+            Collections.swap(favoriteCityList, fromPosition, toPosition)
+            recyclerView.adapter?.notifyItemMoved(fromPosition, toPosition)
+            return true
+        }
+
+        override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+            //called when item is swiped
+            val position = viewHolder.adapterPosition
+            val deletedCity: City = favoriteCityList[position]
+
+            deleteItem(position)
+            updateCityList(deletedCity, false)
+
+            Snackbar.make(recyclerView, "Deleted!", Snackbar.LENGTH_LONG).setAction("UNDO") {
+                undoDelete(position, deletedCity)
+                updateCityList(deletedCity, true)
+            }.show()
+        }
+    })
+
+    private fun updateCityList(deletedCity: City, isFavorite: Boolean) {
+        val cityList = VacationSpots.cityList!!
+        val position = cityList.indexOf(deletedCity)
+        cityList[position].isFavorite = isFavorite
+    }
+
+    private fun deleteItem(position: Int) {
+        favoriteCityList.removeAt(position)
+        favoriteAdapter.notifyItemRemoved(position)
+        favoriteAdapter.notifyItemRangeChanged(position, favoriteCityList.size)
+    }
+
+    private fun undoDelete(position: Int, deletedCity: City) {
+        favoriteCityList.add(deletedCity)
+        favoriteAdapter.notifyItemInserted(position)
+        favoriteAdapter.notifyItemRangeChanged(position, favoriteCityList.size)
+    }
+}
